979cbe767c84c7b44506ebf4e07ebff102bf5dcf
Marcos Vinícius Moraes Costa <81113421+MarcosVini753@users.noreply.github.com>
2024-11-15 21:41:59 -0500

    Update C++.cpp

---
+++ Questões/UVA/UVA 1124/C++.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define SPEED ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define endl "\n"
+
+int main(){ SPEED
+    string equation;
+    while (getline(cin, equation)){
+        cout << equation << endl;
+    }
+    return 0;
+}
